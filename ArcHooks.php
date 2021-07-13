fc219df193eb268ff4e10c81ee3ed1e809324071
Kenny2github <kenny2minecraft@gmail.com>
2021-07-13 22:10:57 +0000

    Finish extension, I think

---
+++ ArcHooks.php
@@ -0,0 +1,19 @@
+<?php
+
+define( 'ARCCDN_SESSION_KEY', 'arccdn-rollout-roll' );
+
+class ArcHooks {
+	public function addArcScript( OutputPage $out, $skin ) {
+		global $wgArcWidgetID, $wgArcRolloutPercentage, $wgRequest;
+		$session = $wgRequest->getSession();
+		if ( !$session->exists( ARCCDN_SESSION_KEY ) ) {
+			$session->set( ARCCDN_SESSION_KEY, rand( 1, 100 ) );
+		}
+		$roll = $session->get( ARCCDN_SESSION_KEY );
+		if ( $roll <= $wgArcRolloutPercentage ) {
+			$out->addHeadItem( 'arccdn-widget', <<<EOS
+<script async src="https://arc.io/widget.min.js#$wgArcWidgetID"></script>
+EOS );
+		}
+	}
+}
