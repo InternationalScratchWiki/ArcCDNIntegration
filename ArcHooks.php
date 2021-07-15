b3845367d907cd79b8d4af77c217d4db1c5f7716
Kenny2github <kenny2minecraft@gmail.com>
2021-07-15 18:40:06 +0000

    Disable preferences logic if rollout is 100%

--- ArcHooks.php
+++ ArcHooks.php
@@ -7,24 +7,26 @@ class ArcHooks {
 	public static function addArcScript( OutputPage $out, $skin ) {
 		global $wgArcWidgetID, $wgArcRolloutPercentage;
 		if ( !$wgArcWidgetID ) return;
-		$lookup = MediaWiki\MediaWikiServices::getInstance()->getUserOptionsLookup();
-		$pref = $lookup->getBoolOption( $skin->getUser(), ARCCDN_PREFERENCE );
 		if ( $wgArcRolloutPercentage == 100 ) {
 			$roll = 0;
 			$comment = wfMessage( 'arccdn-comment-rollout-everyone' )->escaped();
-		} else if ( $pref ) {
-			$roll = 0;
-			$comment = wfMessage( 'arccdn-comment-opted-in' )->escaped();
 		} else {
-			$session = $skin->getRequest()->getSession();
-			if ( !$session->exists( ARCCDN_SESSION_KEY ) ) {
-				$session->set( ARCCDN_SESSION_KEY, rand( 1, 100 ) );
+			$lookup = MediaWiki\MediaWikiServices::getInstance()->getUserOptionsLookup();
+			$pref = $lookup->getBoolOption( $skin->getUser(), ARCCDN_PREFERENCE );
+			if ( $pref ) {
+				$roll = 0;
+				$comment = wfMessage( 'arccdn-comment-opted-in' )->escaped();
+			} else {
+				$session = $skin->getRequest()->getSession();
+				if ( !$session->exists( ARCCDN_SESSION_KEY ) ) {
+					$session->set( ARCCDN_SESSION_KEY, rand( 1, 100 ) );
+				}
+				$roll = $session->get( ARCCDN_SESSION_KEY );
+				$comment = wfMessage(
+					'arccdn-comment-rollout-percentage',
+					$roll, $wgArcRolloutPercentage
+				)->escaped();
 			}
-			$roll = $session->get( ARCCDN_SESSION_KEY );
-			$comment = wfMessage(
-				'arccdn-comment-rollout-percentage',
-				$roll, $wgArcRolloutPercentage
-			)->escaped();
 		}
 		$comment = wfMessage( 'arccdn-comment' )->rawParams( $comment )->escaped();
 		if ( $roll <= $wgArcRolloutPercentage ) {
@@ -36,6 +38,8 @@ EOS );
 	}
 
 	public static function onGetPreferences( $user, &$preferences ) {
+		global $wgArcWidgetID, $wgArcRolloutPercentage;
+		if ( !$wgArcWidgetID || $wgArcRolloutPercentage == 100 ) return;
 		$preferences[ARCCDN_PREFERENCE] = [
 			'type' => 'toggle',
 			'section' => 'rendering',
