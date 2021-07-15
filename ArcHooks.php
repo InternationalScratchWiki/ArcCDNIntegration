778b8a7b813b0b602b543f7bae5eadd7ab843874
Kenny2github <kenny2minecraft@gmail.com>
2021-07-15 17:44:10 +0000

    A couple of QoL changes
    * Get request from $skin, not global
    * Don't bother with session logic if the rollout is 100%
    + Add comment disclosing session roll

--- ArcHooks.php
+++ ArcHooks.php
@@ -4,14 +4,20 @@ define( 'ARCCDN_SESSION_KEY', 'arccdn-rollout-roll' );
 
 class ArcHooks {
 	public static function addArcScript( OutputPage $out, $skin ) {
-		global $wgArcWidgetID, $wgArcRolloutPercentage, $wgRequest;
-		$session = $wgRequest->getSession();
-		if ( !$session->exists( ARCCDN_SESSION_KEY ) ) {
-			$session->set( ARCCDN_SESSION_KEY, rand( 1, 100 ) );
+		global $wgArcWidgetID, $wgArcRolloutPercentage;
+		if ( !$wgArcWidgetID ) return;
+		if ( $wgArcRolloutPercentage == 100 ) {
+			$roll = 0;
+		} else {
+			$session = $skin->getRequest()->getSession();
+			if ( !$session->exists( ARCCDN_SESSION_KEY ) ) {
+				$session->set( ARCCDN_SESSION_KEY, rand( 1, 100 ) );
+			}
+			$roll = $session->get( ARCCDN_SESSION_KEY );
 		}
-		$roll = $session->get( ARCCDN_SESSION_KEY );
 		if ( $roll <= $wgArcRolloutPercentage ) {
 			$out->addHeadItem( 'arccdn-widget', <<<EOS
+<!-- adding Arc widget as $roll <= $wgArcRolloutPercentage -->
 <script async src="https://arc.io/widget.min.js#$wgArcWidgetID"></script>
 EOS );
 		}
